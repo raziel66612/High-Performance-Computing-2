c027c6b59fbb64d9ba93b1e9e9c905319d1837ba
raziel66612 <aniketvishwakarma@gmail.com>
2023-03-31 01:35:15 -0400

    Matrix form Done

--- 1_1_new_algo_matrixForm/algo.c
+++ 1_1_new_algo_matrixForm/algo.c
@@ -5,15 +5,15 @@ static char help[] = " in process ";
 int main(int argc,char **args)
 {
 
-PetscInt m=3, n=3;
+PetscInt m=4, n=4;
 PetscRandom rann;
 Vec a,b;
 Vec c,d; //Vectors to perform orthogonality
 PetscBool flg;
 
 PetscInt i=0,j=0;
-PetscReal dot_prod,normV;
-Mat A;
+PetscReal dot_prod,normV,temp;
+Mat A,Q;
 PetscScalar* xvals;
 PetscScalar value;
 PetscInt sstart,eend;
@@ -21,7 +21,6 @@ PetscInt sstart,eend;
 
 PetscInitialize(&argc, &args, NULL, help);
 MPI_Comm comm = PETSC_COMM_WORLD;  
-
 
 PetscCall( PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-m",&m,&flg));
 PetscCall( PetscOptionsGetInt(PETSC_NULL,PETSC_NULL,"-n",&n,&flg));
@@ -34,7 +33,7 @@ PetscCall( PetscRandomSetFromOptions(rann));
 PetscCall(VecCreate(comm,&a));
 PetscCall(VecSetSizes(a,PETSC_DECIDE,n));
 PetscCall(VecSetFromOptions(a));
-
+//Duplicate Vectors
 PetscCall(VecDuplicate(a,&b));
 PetscCall(VecDuplicate(a,&c));
 PetscCall(VecDuplicate(a,&d));
@@ -49,14 +48,18 @@ PetscCall(MatSetRandom(A,rann));
 MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);  
 MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); 
 
-PetscCall(PetscPrintf(comm,"\nRandom generated Matrix is:\n"));
-MatView(A,PETSC_VIEWER_STDOUT_WORLD);
+PetscCall(PetscPrintf(comm,"\nRandom generated Matrix A:\n"));
+MatView(A,PETSC_VIEWER_STDOUT_WORLD); //print matrix A
 
+//Duplicate Matrix
+PetscCall(MatDuplicate(A,MAT_COPY_VALUES,&Q)); //Duplicate matrix,
 
-//orthogonalizing
+PetscCall(PetscPrintf(comm,"\nDuplicating Matrix A; Matrix Q:\n"));
+MatView(Q,PETSC_VIEWER_STDOUT_WORLD); //print matrix Q
+
+//orthogonalizing: Part 1
 for (i=0; i<n; i++)
 {
-       // Vec a;
         PetscCall(MatGetColumnVector(A,a,i));
         PetscCall(VecAssemblyBegin(a));
         PetscCall(VecAssemblyEnd(a));
@@ -66,44 +69,68 @@ for (i=0; i<n; i++)
     for(j=0; j<i; j++)
     {
          // Vec b;
-        PetscCall(MatGetColumnVector(A,b,j));
+        PetscCall(MatGetColumnVector(Q,b,j));
+        PetscCall(VecNorm(b, NORM_2, &normV));
         PetscCall(VecDot(a,b,&dot_prod));
-        PetscCall(VecAXPY(a,-dot_prod,b));  //Computes [y = alpha x + y] ..... [VecAXPY(Vec y, PetscScalar alpha,Vec x)]  
+        temp=dot_prod/normV/normV;
+        PetscCall(VecAXPY(a,-temp,b));  //Computes [y = alpha x + y] ..... [VecAXPY(Vec y, PetscScalar alpha,Vec x)]  
     }
         PetscCall(VecAssemblyBegin(a));
         PetscCall(VecAssemblyEnd(a));
 
     //normalizing
     PetscCall(VecNorm(a, NORM_2, &normV));
-    PetscCall(VecScale(a, 1.0/normV));
+    PetscCall(VecScale(a, 1.0/(normV*normV)));
     // PetscCall(VecSetValue(a, i, normV, INSERT_VALUES));
 
     PetscCall(VecGetArray(a,&xvals));
-    
     VecGetOwnershipRange(a,&sstart,&eend);
     for (PetscInt k=sstart; k<eend; k++)
     {
-    PetscCall(value=xvals[k]);
-    PetscCall(MatSetValues(A,1,&k,1,&i,&value,INSERT_VALUES));
-    }
-    
+    value=xvals[k];
+    PetscCall(MatSetValues(Q,1,&k,1,&i,&value,INSERT_VALUES));
+    }   
 }
-    MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);  
-    MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); 
+    MatAssemblyBegin(Q,MAT_FINAL_ASSEMBLY);  
+    MatAssemblyEnd(Q,MAT_FINAL_ASSEMBLY); 
 
-PetscCall(PetscPrintf(comm,"\nNormalized Random Matrix:\n"));
-MatView(A,PETSC_VIEWER_STDOUT_WORLD); //--------------------------Print MAT-------------------
+//orthogonalizing: Part 2
+for (i=n-1; i>=0; i--)
+{
+        PetscCall(MatGetColumnVector(Q,a,i));
+        PetscCall(VecAssemblyBegin(a));
+        PetscCall(VecAssemblyEnd(a));
+    for(j=n-1; j>i; j--){
+        PetscCall(MatGetColumnVector(A,b,j));
+        PetscCall(MatGetColumnVector(Q,c,j));
+        PetscCall(VecNorm(b, NORM_2, &normV));
+        PetscCall(VecDot(a,b,&dot_prod));
+        PetscCall(VecAXPY(a,-dot_prod,c));  //Computes [y = alpha x + y] ..... [VecAXPY(Vec y, PetscScalar alpha,Vec x)]  
+    }
+        PetscCall(VecAssemblyBegin(a));
+        PetscCall(VecAssemblyEnd(a));
+
+    PetscCall(VecGetArray(a,&xvals));
+    VecGetOwnershipRange(a,&sstart,&eend);
+    for (PetscInt k=sstart; k<eend; k++)
+    {
+    value=xvals[k];
+    PetscCall(MatSetValues(Q,1,&k,1,&i,&value,INSERT_VALUES));
+    }   
+}
+    MatAssemblyBegin(Q,MAT_FINAL_ASSEMBLY);  
+    MatAssemblyEnd(Q,MAT_FINAL_ASSEMBLY); 
+
+PetscCall(PetscPrintf(comm,"\nAfter Orthogonalizing, Matrix Q:\n"));
+MatView(Q,PETSC_VIEWER_STDOUT_WORLD); //print matrix Q
 
 //Orthogonality Check
-PetscCall( PetscPrintf(PETSC_COMM_WORLD, "\n \n") );   //Print ---------------------------
+PetscCall( PetscPrintf(PETSC_COMM_WORLD, "\n[A*Q = I]: \n") );   //Print ---------------------------
 for (i=0; i<n; i++) {
-    PetscCall(MatGetColumnVector(A,d,i));
-    // PetscCall(VecView(c,PETSC_VIEWER_STDOUT_WORLD));//Print --------------
-
-
+PetscCall(MatGetColumnVector(A,d,i));
     for (j=0; j<n; j++) {
         PetscScalar dot;
-        PetscCall(MatGetColumnVector(A,c,j));
+        PetscCall(MatGetColumnVector(Q,c,j));
         PetscCall(VecAssemblyEnd(d));
 
         PetscCall( VecDot(c,d,&dot) );
@@ -114,6 +141,7 @@ for (i=0; i<n; i++) {
 }
 
 PetscCall(MatDestroy(&A));
+PetscCall(MatDestroy(&Q));
 PetscCall(VecDestroy(&a));
 PetscCall(VecDestroy(&b));
 PetscCall(VecDestroy(&c));
@@ -123,7 +151,7 @@ PetscCall(VecDestroy(&d));
 PetscCall(PetscRandomDestroy(&rann));   
 
 PetscFinalize();
-printf("\nThats all folks\n");
+printf("\nThats all\n");
  
     return 0;
 }
